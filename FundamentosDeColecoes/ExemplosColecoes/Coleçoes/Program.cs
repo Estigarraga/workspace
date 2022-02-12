73d9b5ab3f77d93e7416b681a8e1dd4fbbd6dd70
Estigarraga <e.estigarraga@gmail.com>
2022-02-12 07:11:44 -0300

    CursoFundamentosDeColecoes1

---
+++ FundamentosDeColecoes/ExemplosColecoes/Coleçoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInteiros = new int[10];
+
+            arrayInteiros[8] = 3510;
+            arrayInteiros[0] = 4560;
+            arrayInteiros[9] = 5000;
+
+            System.Console.WriteLine("Percorrendo array pelo for");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                System.Console.WriteLine(arrayInteiros[i]);
+            }
+
+            System.Console.WriteLine("Percorrendo array pelo foreach");
+            foreach (var item in arrayInteiros)
+            {
+                System.Console.WriteLine(item);
+            }
+
+        }
+    }
+}
